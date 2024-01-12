dcc1d57c835b9bc347924b126fe6303134753314
surbina <sheyla.ulopez@gmail.com>
2024-01-11 19:44:49 -0600

    Terminando de configurar productos con commons

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServiciosProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServiciosProductosApplication.java
@@ -2,10 +2,14 @@ package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.formacionbdi.springboot.app.commons.models.entity"})//para que busque la libreria commons que contiene el entity productos en el proyecto de springboot-servicio-commons
+//una vez que se borró del proyecto la carpeta con la clase de productos.java
+//podemos separar por coma n cantidad de packages
 public class SpringbootServiciosProductosApplication {
 
 	public static void main(String[] args) {
